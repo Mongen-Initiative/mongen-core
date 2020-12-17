3bcf9e01e7047f0dbff160f5be22a3182e6c40f7
Juan Negrier <juannegrier@gmail.com>
2020-12-17 00:17:49 +0000

    Added CORS exception for local frontend service

--- src/main/java/org/mongen/core/MongenCoreService.java
+++ src/main/java/org/mongen/core/MongenCoreService.java
@@ -2,12 +2,26 @@ package org.mongen.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class MongenCoreService {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MongenCoreService.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/v1/countries").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
